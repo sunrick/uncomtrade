3b8e1f9f1d562096b1d3386a9d70fae4cd71a35d
Rickard Sunden <rickard.sunden@outlook.com>
2015-10-25 20:45:21 -0400

    First working version of request class.

--- lib/uncomtrade/request.rb
+++ lib/uncomtrade/request.rb
@@ -1,6 +1,33 @@
+require 'pry'
+
 module Uncomtrade
 
   class Request
+
+    def initialize(opts={})
+      @options = Options.new(opts)
+      @query = Query.new
+    end
+
+    def get_data
+      query.get_data(options.list_options)
+    end
+
+    def reset
+      options.reset
+    end
+
+    def update(opts={})
+      options.update(options)
+    end
+
+    def list_options
+      options.list_options
+    end
+
+    private
+    attr_reader :options, :query
+
   end
 
 end
