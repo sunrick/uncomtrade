3ec547504e207c334abf747b3b92700a1484c7dc
Rickard Sunden <rickard.sunden@outlook.com>
2015-10-25 21:23:11 -0400

    Basic methods for retrieving data.

--- lib/uncomtrade/response.rb
+++ lib/uncomtrade/response.rb
@@ -1,6 +1,29 @@
 module Uncomtrade
 
-  class Request
+  class Response
+
+    def initialize(response)
+      @response = JSON.parse(response.body)
+    end
+
+    def result
+      response
+    end
+    
+    def dataset
+      response["dataset"]
+    end
+
+    def iterate(array=[])
+      unless array.empty?
+
+      end
+    end
+
+    private
+
+    attr_reader :response
+
   end
 
 end
