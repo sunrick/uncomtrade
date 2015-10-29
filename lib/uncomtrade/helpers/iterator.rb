1355fca625a7ce41b3127e644175c78996838f1b
Rickard Sunden <rickard.sunden@outlook.com>
2015-10-28 23:12:49 -0400

    Add to_csv support.

--- lib/uncomtrade/helpers/iterator.rb
+++ lib/uncomtrade/helpers/iterator.rb
@@ -8,9 +8,23 @@ module Uncomtrade
       end
 
       def cherry_pick(selectors)
-        selectors = syms_to_strings(selectors)
-        array.map do |data|
-          hash = create_hash(data, selectors)
+        if !selectors.empty?
+          selectors = syms_to_strings(selectors)
+          array.map do |data|
+            hash = create_hash(data, selectors)
+          end
+        else
+          array
+        end
+      end
+
+      def to_csv(csv_path, selectors)
+        CSV.open(csv_path, "wb") do |csv|
+          picked_array = cherry_pick(selectors)
+          csv << picked_array.first.keys
+          picked_array.each do |hash|
+            csv << hash.values
+          end
         end
       end
 
--- spec/helpers/iterator_spec.rb
+++ spec/helpers/iterator_spec.rb
@@ -25,4 +25,38 @@ RSpec.describe Uncomtrade::Helpers::Iterator do
                          {"period" => 2014, "rtTitle" => "Albania"}])
   end
 
+  it 'passing an empty array should return the original array' do
+    iterator = Uncomtrade::Helpers::Iterator.new(data)
+    array = iterator.cherry_pick([])
+    expect(array).to eq(data)
+  end
+
+  it 'should write a csv file and with no selectors and data should match' do
+    iterator = Uncomtrade::Helpers::Iterator.new(data)
+    iterator.to_csv('data.csv', [])
+    file = csv_crap
+    expect(file).to eq(data)
+  end
+
+  it 'should write a csv file and with selectors and data should match' do
+    iterator = Uncomtrade::Helpers::Iterator.new(data)
+    selectors = [:period]
+    iterator.to_csv('data.csv', selectors)
+    array = iterator.cherry_pick(selectors)
+    file = csv_crap
+    expect(file).to eq(array)
+  end
+
+  ## HELPER METHODS
+
+  def csv_crap
+    CSV.new(File.read('data.csv'), headers: true, converters: :all).to_a.map do |row|
+      new_hash = row.to_hash
+      # LOL, ONE NUMBER IN JSON RESPONSE IS A STRING.
+      # OMG I HATE LIFE
+      new_hash["periodDesc"] = new_hash["periodDesc"].to_s if new_hash["periodDesc"]
+      new_hash
+    end
+  end
+
 end
