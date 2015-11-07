a2076bfa953eba50ba3adaa90ed2f71ebc48fb39
Rickard Sunden <rickard.sunden@outlook.com>
2015-11-07 09:10:40 -0500

    Add csv method.

--- lib/uncomtrade/response.rb
+++ lib/uncomtrade/response.rb
@@ -6,6 +6,7 @@ module Uncomtrade
 
     def initialize(response)
       @response = JSON.parse(response.body)
+      @iterator = Helpers::Iterator.new(dataset)
     end
 
     def result
@@ -17,13 +18,16 @@ module Uncomtrade
     end
 
     def cherry_pick(*selectors)
-      iterator = Helpers::Iterator.new(dataset)
       iterator.cherry_pick(selectors)
     end
 
+    def to_csv(csv_path, *selectors)
+      iterator.to_csv(csv, selectors)
+    end
+
     private
-    
-    attr_reader :response
+
+    attr_reader :response, :iterator
 
   end
 
